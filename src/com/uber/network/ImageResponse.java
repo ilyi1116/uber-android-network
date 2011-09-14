3a567899144bdd83c5db5d26d662d3d395ca5dd8
Victor <vitchor@gmail.com>
2011-09-13 20:51:54 -0700

    Added orientation change on images

--- src/com/uber/network/ImageResponse.java
+++ src/com/uber/network/ImageResponse.java
@@ -23,28 +23,18 @@
 
 package com.uber.network;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 
+import com.uber.utils.ExifUtil;
+
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 
 public class ImageResponse extends Response {
 	
 	private Bitmap mBitmap;
 	
 	public ImageResponse(InputStream data) throws ResponseException {
-        try {
-        	BufferedInputStream bis = new BufferedInputStream(data); 
-			mBitmap = BitmapFactory.decodeStream(bis);
-			bis.close();
-		} catch (IOException e) {
-			throw new ResponseException();
-		} catch (OutOfMemoryError e) {
-			System.gc();
-			throw new ResponseException();
-		}
+		mBitmap = ExifUtil.fixImageOrientation(data);
 	}
 
 	@Override
